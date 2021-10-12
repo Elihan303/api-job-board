b8a871c6c612d48e38e47660aea783712b7bd853
Elihan303 <elihanth@hotmail.com>
2021-10-11 22:35:49 -0400

    Heroku

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 //REQUIRE
-const express = require('express')
+ import express from 'express';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import path from 'path';
+import cors from 'cors';   
 const app = express();
-const morgan = require('morgan');
-const mongoose= require('mongoose');
-const path= require('path');
-
 
 //CONNECTING TO DB
 mongoose.connect('mongodb://localhost/job_board')
@@ -13,24 +13,34 @@ mongoose.connect('mongodb://localhost/job_board')
 
 //SETTINGS
 app.set('port',process.env.PORT || 4000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine','ejs');
 
 
-//MIDDLEWARES 
+//MIDDLEWARES
 
+//Comunicacion entre multiples servers
+app.use(cors());
 //muestra que esta pasando con las peticiones
-app.use(morgan('dev'));
+app.use(morgan('tiny'));
 //Sirve para interpretar los datos que viene de un formulario
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended:true}));
 //Sirve para entender json
 app.use(express.json());
 
 //ROUTES
+
 app.use('/api/createjob', require('./routes/create_job.js'))
 app.use('/api/jobinfo', require('./routes/job_info.js'))
 app.use('/api/viewjob', require('./routes/view_job'))
 app.use('/api/login', require('./routes/login_job.js'))
+
+
+
+
+// Middleware para Vue.js router modo history
+const history=  require ('connect-history-api-fallback');
+app.use(history());
+//acceder al directorio actual
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
