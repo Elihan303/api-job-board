ff2a826771d736f85d2514761d26909408583071
Elihan303 <elihanth@hotmail.com>
2021-10-17 21:29:14 -0400

    v0.1

--- src/models/jobs.js
+++ src/models/jobs.js
@@ -1,16 +1,23 @@
 import mongoose from 'mongoose';
 const Schema= mongoose.Schema;
 
+
+const tipos={
+    values:['full-time', 'part-time', 'freelance'],
+    message:'{vale} tipo no valido'
+}
+
 //creamos modelo
 const job_Schema= new Schema({
-    Posicion: String,
-    Descripcion: String,
-    Ubicacion: String,
+    UsuarioID:{typ:String, required:['true','ID obligatorio']},
+    Posicion: {type: String, required:[true,'Posicion obligatoria']},
+    Descripcion: {type: String, required:[true,'Descripcion obligatoria']},
+    Ubicacion: {type: String, required:[true,'Ubicacion obligatoria']},
     URL: String,
-    Tipo: String,
-    Categoria: String,
-    Compañia: String,
-    Fecha: Date
+    Tipo: {type: String, required:[true,'Tipo obligatoria'],enum: tipos},
+    Categoria: {type: String, required:[true,'Categoria obligatoria']},
+    Compañia: {type: String, required:[true,'Compañia obligatoria']}
+    //Fecha: {type: Date, default: Date.now}
 })
 
 //module.exports= mongoose.model('puesto_ofertas', stalls_offers_Schema);
--- src/routes/job.js
+++ src/routes/job.js
@@ -4,9 +4,13 @@ const router = express.Router();
 //import schema
 import job from '../models/jobs'
 
+const { verificarAuth, verificarAdmin,verificarPoster } = require('../middlewares/authentication');
+
 //crear trabajo
-router.post('/', async (req, res) => {
+router.post('/',[verificarAuth,verificarPoster,verificarAdmin], async (req, res) => {
     const body = req.body
+    body.UsuarioID= req.user._id;
+
     try {
         const jobDB = await job.create(body);
         res.status(200).json(jobDB);
@@ -23,7 +27,7 @@ router.post('/', async (req, res) => {
 })
 
 //Editar trabajo
-router.put('/:id', async (req, res) => {
+router.put('/:id',[verificarAuth,verificarPoster,verificarAdmin], async (req, res) => {
     const _id = req.params.id;
     const body = req.body
     try {
@@ -56,7 +60,7 @@ router.get('/', async (req, res) => {
 
 })
 //obtener un trabajo por id
-router.get('/:id', async (req, res) => {
+router.get('/:id',verificarAuth, async (req, res) => {
     try {
         const _id = req.params.id;
         const jobDB = await job.findOne({ _id });
@@ -72,8 +76,26 @@ router.get('/:id', async (req, res) => {
 
 })
 
+//obtener el cantidad de datos que especifique el admin
+router.get('/view/:cant', verificarAuth, async(req,res)=>{
+    try {
+        const cant = req.params.cant; 
+        const jobDB = await job.findOne({ }).limit(cant);
+        res.status(200).json(jobDB);
+    } catch (error) {
+        return res.status(400).json(
+            {
+                mensaje: 'Ocurrio un error',
+                error
+            }
+        )
+    }
+})
+
+
+
 //eliminar trabajo
-router.delete('/:id', async (req, res) => {
+router.delete('/:id',[verificarAuth,verificarPoster,verificarAdmin], async (req, res) => {
     try {
         const _id = req.params.id;
         const jobDB = await users.findByIdAndDelete({ _id });
